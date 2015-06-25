471c12212d32f6a7f4af0ee0f544118252e22b88
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-06-25 14:08:31 -0700

    Updated the git prompt to be colorized based on the current state.

--- src/scripts/set-prompt.sh
+++ src/scripts/set-prompt.sh
@@ -1,13 +1,11 @@
 # make sure that git prompt is sourced first
 source ~/.ssh/scripts/git-prompt.sh
 
-function set-prompt() {
-    # set the window title
-    echo -ne "\033]0;${USER}@${HOSTNAME%%.*} ${PWD}\007"
-	
+function emit-prompt-arrow()
+{
     # set the default color
     local CLR_PROMPT=$CLR_USER_PROMPT
-	
+
     # set color for staff user (administrator)
     if [ ! -z '`command id -Gn | grep -s -o admin`' ]; then
         CLR_PROMPT=$CLR_STAFF_PROMPT
@@ -18,14 +16,23 @@ function set-prompt() {
         CLR_PROMPT=$CLR_ROOT_PROMPT
     fi
 
+    echo "\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
+}
+
+function set-prompt() {
+    # set the window title
+    echo -ne "\033]0;${USER}@${HOSTNAME%%.*} ${PWD}\007"
+
+    export PROMPT_COMMAND='__git_ps1 "\n\u@\h : \w\n" "`emit-prompt-arrow`"'
+
     # specify the two-line prompt with color-coded arrow
-    export PS1="\n\u@\h : \w\n  $(__git_ps1 '(%s)')\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
+    # export PS1="\n\u@\h : \w\n  $(__git_ps1 '(%s)')\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
 }
 
 GIT_PS1_SHOWDIRTYSTATE=true
 GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWCOLORHINTS=true
 GIT_PS1_SHOWUNTRACKEDFILES=true
 GIT_PS1_SHOWUPSTREAM="verbose"
 
-# export the prompt command
-export PROMPT_COMMAND=set-prompt
+set-prompt
---
+++ src/scripts/show-profile.sh
@@ -0,0 +1,11 @@
+function show-profile() {
+    history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail -n 20 | sort -nr
+}
+
+function showprofile() {
+    show-profile
+}
+
+function myprofile() {
+    show-profile
+}
