931ec4241f09ae766de06f4ff842b3112e3a8b8e
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-06-25 00:39:12 -0700

    Updated the theme, set-prompt, and bashrc to change the colors around a bit

--- src/scripts/set-prompt.sh
+++ src/scripts/set-prompt.sh
@@ -2,24 +2,24 @@
 source ~/.ssh/scripts/git-prompt.sh
 
 function set-prompt() {
-	# set the window title
-	echo -ne "\033]0;${USER}@${HOSTNAME%%.*} ${PWD}\007"
+    # set the window title
+    echo -ne "\033]0;${USER}@${HOSTNAME%%.*} ${PWD}\007"
 	
-	# set the default color
-	local CLR_PROMPT=$CLR_USER_PROMPT
+    # set the default color
+    local CLR_PROMPT=$CLR_USER_PROMPT
 	
-	# set color for root user
-	if [ `whoami` == 'root' ]; then
-		CLR_PROMPT=$CLR_ROOT_PROMPT
-	fi
-	
-	# set color for staff user (administrator)
-	if [ ! -z '`command id -Gn | grep -s -o admin`' ]; then
-	   CLR_PROMPT=$CLR_STAFF_PROMPT
-	fi
+    # set color for staff user (administrator)
+    if [ ! -z '`command id -Gn | grep -s -o admin`' ]; then
+        CLR_PROMPT=$CLR_STAFF_PROMPT
+    fi
 
-	# specify the two-line prompt with color-coded arrow
-	export PS1="\n\u@\h : \w\n  $(__git_ps1 '(%s)')\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
+    # set color for root user
+    if [ $(id -u) -eq 0 ]; then
+        CLR_PROMPT=$CLR_ROOT_PROMPT
+    fi
+
+    # specify the two-line prompt with color-coded arrow
+    export PS1="\n\u@\h : \w\n  $(__git_ps1 '(%s)')\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
 }
 
 GIT_PS1_SHOWDIRTYSTATE=true
