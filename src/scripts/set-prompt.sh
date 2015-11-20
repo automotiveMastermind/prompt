ff4a33c3f51efdcbca17aca8c6edfa451aeef10f
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-11-20 15:47:28 -0800

    feat(prompt): indicate (ssh) on prompt when connected to a remote
    note: uber must be installed on the remote -- the prompt rendered when connected to
    an ssh session is the remote prompt, not the local one. there is no way to automatically
    override this prompt from the local side.

--- src/scripts/set-prompt.sh
+++ src/scripts/set-prompt.sh
@@ -15,18 +15,25 @@ function emit-prompt-arrow()
     if [ $(id -u) -eq 0 ]; then
         CLR_PROMPT=$CLR_ROOT_PROMPT
     fi
-
-    echo "\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
+    
+    # set the color for an ssh session
+    if [ "$SSH_CONNECTION" ]; then
+        CLR_PROMPT=$CLR_SSH_PROMPT
+        
+        # emit the prompt using the correct color
+        echo "\[${CLR_PROMPT}\] (ssh) ==> \[${CLR_CLEAR}\]"
+    else
+        # emit the prompt using the correct color
+        echo "\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
+    fi
 }
 
 function set-prompt() {
     # set the window title
     echo -ne "\033]0;${USER}@${HOSTNAME%%.*} ${PWD}\007"
 
+    # use the git prompt with the prompt arrow
     export PROMPT_COMMAND='__git_ps1 "\n\u@\h : \w\n" "`emit-prompt-arrow`"'
-
-    # specify the two-line prompt with color-coded arrow
-    # export PS1="\n\u@\h : \w\n  $(__git_ps1 '(%s)')\[${CLR_PROMPT}\] ==> \[${CLR_CLEAR}\]"
 }
 
 set-prompt
