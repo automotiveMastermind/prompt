f4e8db28e1ae799da7ce1e515813aa440e9dea9e
Deavon M. McCaffery <dmccaffery@pulsebridge.com>
2015-12-11 13:31:03 -0800

    fix(git): execute git flow init instead of git init in git-extensions

--- src/scripts/git-extensions.sh
+++ src/scripts/git-extensions.sh
@@ -19,13 +19,15 @@ git-clone() {
 	
 	git clone $url $name && \
 	cd $name && \
-	git branch -a | sed -n "/\/HEAD /d; /\/master$/d; /\/develop$/d; /remotes/p;" | xargs -L1 git checkout -t 2>/dev/null && \
-	git init -d 2>/dev/null
+	git branch -a | sed -n "/\/HEAD /d; /\/master$/d; /\/develop$/d; /remotes/p;" | xargs -L1 git checkout -t 2>/dev/null
 	
 	echo Checking out master and develop branches...
 	
 	git checkout develop 2>/dev/null
+	git checkout -b master 2>/dev/null
 	git checkout master 2>/dev/null
+
+	git flow init -d 2>/dev/null
 }
 
 clone() {
