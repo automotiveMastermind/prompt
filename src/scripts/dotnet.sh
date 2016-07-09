3dcdfef298505bccdbdb82b94f4dd186afe9954a
Deavon McCaffery <dmccaffery@pulsebridge.com>
2016-07-08 19:48:58 -0700

    fix(dotnet): ensure dotnet is on the path

--- src/scripts/dotnet.sh
+++ src/scripts/dotnet.sh
@@ -8,31 +8,28 @@ function install-dotnet() {
     local dotnetsh=$dotnetpath/dotnet-install.sh
     
     # determine if dotnet is already on the path
-    if test type dotnet 1>/dev/null 2>&1; then
-        return
+    if ! type dotnet 1>/dev/null 2>&1; then
+        # determine if the dotnet folder exists
+        if test ! -d "$dotnetpath"; then
+            # create the dotnet folder
+            mkdir -p "$dotnetpath"
+        fi
+
+        # determine if the dotnet script file does not exist
+        if test ! -f "$dotnetsh"; then
+            # attempt to download it from curl
+            local result=$(curl -L -D - "$dotneturi" -o "$dotnetsh" -# | grep "^HTTP/1.1" | head -n 1 | sed "s/HTTP.1.1 \([0-9]*\).*/\1/")
+
+            # source it if it was successfully retrieved
+            test "$result" = "200" && chmod ugo+x "$dotnetsh"
+        fi
     fi
     
-    # determine if the dotnet folder exists
-    if test ! -d "$dotnetpath"; then
-        # create the dotnet folder
-        mkdir -p "$dotnetpath"
-    fi
-
-    # determine if the dotnet script file does not exist
-    if test ! -f "$dotnetsh"; then
-        # attempt to download it from curl
-        local result=$(curl -L -D - "$dotneturi" -o "$dotnetsh" -# | grep "^HTTP/1.1" | head -n 1 | sed "s/HTTP.1.1 \([0-9]*\).*/\1/")
-
-        # source it if it was successfully retrieved
-        test "$result" = "200" && chmod ugo+x "$dotnetsh"
-    fi
-
     # determine if dotnet now exists in the expected place
     if test -f "$dotnetsh"; then
         # add the dotnet folder to path
         PATH=$PATH:$dotnetpath
-
-        # source dotnet
-        source $dotnetsh
     fi
 }
+
+install-dotnet
