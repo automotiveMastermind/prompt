5167da48d15e0fc2ac4f1d54ba80551fe5551b24
Deavon M. McCaffery <deavon@amastermind.com>
2017-08-31 15:01:22 -0700

    feat(docker): add update for all images

--- src/scripts/docker.sh
+++ src/scripts/docker.sh
@@ -16,3 +16,7 @@ docker-rm() {
 docker-rmi() {
     docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
+
+docker-update() {
+    docker images | grep -v REPOSITORY | awk '{print $1}' | xargs -L1 docker pull
+}
