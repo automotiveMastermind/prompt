b533c3e8263b12a443d124611fb8fb7d184f01e5
Deavon M. McCaffery <deavon@automotivemastermind.com>
2018-04-21 23:38:28 -0700

    fix(list-directory): fix aliases for la, ll, lla, and lsp

--- src/scripts/list-directory.sh
+++ src/scripts/list-directory.sh
@@ -4,26 +4,26 @@ if [ ! -z "${AM_PROMPT_DEBUG:-}" ]; then
     echo 'list-directory'
 fi
 
-if [ ! type "la" 1>/dev/null 2>&1 ]; then
+if ! type "la" 1>/dev/null 2>&1; then
     la() {
         ls -la $@
     }
 fi
 
-if [ ! type "ll" 1>/dev/null 2>&1 ]; then
+if ! type "ll" 1>/dev/null 2>&1; then
     ll() {
         ls -lh $@
     }
 fi
 
-if [ ! type "lla" 1>/dev/null 2>&1 ]; then
+if ! type "lla" 1>/dev/null 2>&1; then
     lla() {
         ls -lha $@
     }
 fi
 
-if [ ! type "lp" 1>/dev/null 2>&1 ]; then
-    lp() {
+if ! type "lsp" 1>/dev/null 2>&1; then
+    lsp() {
         ls -p $@
     }
 fi
