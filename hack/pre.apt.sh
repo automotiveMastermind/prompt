8457545b33387f14d934eae5b563e1d7015b601e
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2021-01-03 05:16:56 +0000

    build(wsl): fix wsl build

--- hack/pre.apt.sh
+++ hack/pre.apt.sh
@@ -5,7 +5,9 @@ set -e
 apt-get update && apt-get install -y gosu sudo git
 gosu nobody true
 
+
 useradd --uid 1000 --user-group --system --create-home --no-log-init --groups tty --shell /bin/bash build
 printf "%s\n" 'build ALL=(ALL) NOPASSWD:ALL' >>/etc/sudoers
+
 chown -R build:build /home/build
 chmod u=rwx,g=rx,o= /home/build
